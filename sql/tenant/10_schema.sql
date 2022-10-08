ca3f2aaa86f82168c76916d0d5afbede06a64c60
samosica <samosic4@gmail.com>
2022-10-09 02:34:02 +0900

    Add an index of player

--- sql/tenant/10_schema.sql
+++ sql/tenant/10_schema.sql
@@ -47,3 +47,4 @@ CREATE TABLE billing_report (
 CREATE INDEX IF NOT EXISTS player_score_tenant_comp_idx ON player_score (tenant_id, competition_id);
 CREATE INDEX IF NOT EXISTS player_score_player_idx ON player_score (player_id);
 CREATE INDEX IF NOT EXISTS player_score_rank_idx ON player_score (competition_id, rank);
+CREATE INDEX IF NOT EXISTS competition_tenant_idx ON competition (tenant_id, created_at DESC);
